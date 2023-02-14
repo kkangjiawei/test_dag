7bb3dc3e2a7213879477b8aa01697c6f719c3ca6
kkangjiawei <40045652+kkangjiawei@users.noreply.github.com>
2023-02-14 19:36:56 +0800

    Update monitoring.py
    Signed-off-by: kkangjiawei <40045652+kkangjiawei@users.noreply.github.com>

--- monitoring.py
+++ monitoring.py
@@ -5,7 +5,7 @@ from ray.serve.handle import RayServeDeploymentHandle
 from ray.serve.drivers import DAGDriver
 from ray.serve.deployment_graph import InputNode
 from typing import Dict, List
-from a import add_one
+from a1 import *
 logger = logging.getLogger("ray.serve")
 
 
